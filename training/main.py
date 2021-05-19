88cef8606ea7fdae9816584090dcd81e14f77acc
Bianca-Cristiana Caluian <bianca.caluian12@gmail.com>
2021-05-19 19:38:08 +0300

    final changes

--- training/main.py
+++ training/main.py
@@ -3,16 +3,16 @@ from training.linear_regression import make_linear_regression_model
 from sklearn.metrics import mean_absolute_error
 
 if __name__ == '__main__':
-    X_train = read_dataset("../preprocessing/dataset/X_train.csv")
-    X_test = read_dataset("../preprocessing/dataset/X_test.csv")
-    Y_train = read_dataset("../preprocessing/dataset/Y_train.csv")
-    Y_test = read_dataset("../preprocessing/dataset/Y_test.csv")
+    X_train = read_dataset("../preprocessing/dataset/X_train2.csv")
+    X_test = read_dataset("../preprocessing/dataset/X_test2.csv")
+    Y_train = read_dataset("../preprocessing/dataset/Y_train2.csv")
+    Y_test = read_dataset("../preprocessing/dataset/Y_test2.csv")
 
-    X_train = X_train.drop(X_train.columns[0], axis=1)
-    X_test = X_test.drop(X_test.columns[0], axis=1)
-
-    Y_train = Y_train.drop(Y_train.columns[0], axis=1)
-    Y_test = Y_test.drop(Y_test.columns[0], axis=1)
+    # X_train = X_train.drop(X_train.columns[0], axis=1)
+    # X_test = X_test.drop(X_test.columns[0], axis=1)
+    #
+    # Y_train = Y_train.drop(Y_train.columns[0], axis=1)
+    # Y_test = Y_test.drop(Y_test.columns[0], axis=1)
 
     pred_baseline = [Y_train.mean()] * len(Y_train)
 
